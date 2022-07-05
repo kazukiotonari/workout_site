693d1269e4b300d24cb25692c5caa94103ca40d8
kazukiotonari <siamdaitalove@gmail.com>
2022-07-05 13:24:06 +0000

    create_login_system

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   }
 
   devise_scope :user do
+    root to: "home#index"
     get "signup", :to => "users/registrations#new"
     get "login", :to => "users/sessions#new"
     delete "logout", :to => "users/sessions#destroy"
